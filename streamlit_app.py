81994d9d1c8208731ef53cc02c6045fc8a954a4e
vlaja011 <vladan@gmail.com>
2025-11-24 14:36:53 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.primitives import serialization
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
@@ -11,7 +15,21 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be: ', name_on_order)
 
-cnx = st.connection("snowflake")
+with open("/Users/vladanstrbac/ssl/rsa_key.p8", "rb") as key:
+    p_key = serialization.load_pem_private_key(
+        key.read(),
+        password=os.environ["PRIVATE_KEY_PASSPHRASE"].encode(),
+        backend=default_backend(),
+    )
+
+pkb = p_key.private_bytes(
+    encoding=serialization.Encoding.DER,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption(),
+)
+
+cnx = st.connection("snowflake", type="snowflake", private_key=pkb)
+
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
